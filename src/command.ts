2b1cef4ce71f2e214cf83accd1cc4a87d7266396
takumi091111 <takumi091111@gmail.com>
2020-07-08 11:47:50 +0900

    リファクタリング メッセージの修正

--- src/command.ts
+++ src/command.ts
@@ -5,33 +5,34 @@ import { openFile, saveFile } from '@/utils/file'
 import { watchGlob } from '@/utils/watch'
 import { CommandProps } from '@/types'
 
-const mainAction = async ({ html }: Omit<CommandProps, 'pattern'>) => {
-  // コマンドを実行したディレクトリからの相対パス
-  const filePath = resolve(process.cwd(), html)
+// コマンドを実行したディレクトリ
+const cwd = process.cwd()
 
+const processFile = async (htmlPath: string) => {
   // HTMLファイルを開いて読み込む
-  const content = await openFile(filePath)
+  const content = await openFile(htmlPath)
   const element = parse(content)
 
   // 変更後に上書きする
   const newHtml = await appendTimeStamp(element)
-  saveFile(filePath, newHtml)
+  saveFile(htmlPath, newHtml)
 }
 
 export const action = async ({ html, watch }: CommandProps) => {
+  const htmlPath = resolve(cwd, html)
+
   if (watch === undefined) {
-    mainAction({ html })
-    console.info('✨ Update completed.')
+    processFile(htmlPath)
+    console.info('✨ Process completed.')
     return
   }
+
   watchGlob(watch, (path) => {
-    const cwd = process.cwd()
-    const htmlPath = resolve(cwd, html)
     const targetPath = resolve(cwd, path)
-    // htmlと同じファイルの更新は無視する(無限ループ対策)
+    // htmlの更新は無視する(無限ループ対策)
     if (htmlPath === targetPath) return
 
-    console.info('✨ Update completed. Watching for file changes: ', path)
-    mainAction({ html })
+    processFile(htmlPath)
+    console.info('✨ Process completed. File changes detected: ', path)
   })
 }
